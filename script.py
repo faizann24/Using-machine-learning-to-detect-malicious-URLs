2eb742d5d3be8be304e532da500a99c81ea3caad
Joerg Stephan <johe.stephan@gmail.com>
2017-02-17 21:07:06 +0100

    adjusted

--- script.py
+++ script.py
@@ -1,12 +1,12 @@
 
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
 import pandas as pd
 import numpy as np
 import random
 from sklearn.feature_extraction.text import TfidfVectorizer
 import sys
-reload(sys)
-sys.setdefaultencoding('utf-8')
+
 from sklearn.linear_model import LogisticRegression
 
 def getTokens(input):
@@ -24,7 +24,7 @@ def getTokens(input):
 		allTokens.remove('com')	#removing .com since it occurs a lot of times and it should not be included in our features
 	return allTokens
 
-allurls = 'C:\\Users\\Faizan Ahmad\\Desktop\\Url Classification Project\\Data to Use\\allurls.txt'	#path to our all urls file
+allurls = './data/data.csv'	#path to our all urls file
 allurlscsv = pd.read_csv(allurls,',',error_bad_lines=False)	#reading file
 allurlsdata = pd.DataFrame(allurlscsv)	#converting to a dataframe
 
@@ -46,5 +46,5 @@ print(lgs.score(X_test, y_test))	#pring the score. It comes out to be 98%
 X_predict = ['wikipedia.com','google.com/search=faizanahad','pakistanifacebookforever.com/getpassword.php/','www.radsport-voggel.de/wp-admin/includes/log.exe','ahrenhei.without-transfer.ru/nethost.exe','www.itidea.it/centroesteticosothys/img/_notes/gum.exe']
 X_predict = vectorizer.transform(X_predict)
 y_Predict = lgs.predict(X_predict)
-print y_Predict	#printing predicted values
+print(y_Predict)	#printing predicted values
 
